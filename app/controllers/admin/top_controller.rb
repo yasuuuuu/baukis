1b9ed6d13dbd95118f125e78c7af0ee63fa26b5e
Yasuhiro Kume <yasu_figure8@yahoo.co.jp>
2016-07-13 01:35:34 +0900

    10章まで

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,6 +1,9 @@
 class Admin::TopController < Admin::Base
   def index
-    render action: 'index'
-    # raise IpAddressRejected
+    if current_administrator
+      render action: 'dashboard'
+    else
+      render action: 'index'
+    end
   end
 end
