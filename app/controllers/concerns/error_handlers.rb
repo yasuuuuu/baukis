4cf4c36b4668367d138a2fddbcc672450f790fea
Yasuhiro Kume <yasu_figure8@yahoo.co.jp>
2016-07-17 11:35:40 +0900

    editまで

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,6 +3,7 @@ module ErrorHandlers
 
   included do
     rescue_from Exception, with: :rescue500
+    rescue_from ActionController::ParameterMissing, with: :rescue400
     rescue_from ApplicationController::Forbidden, with: :rescue403
     rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActionController::RoutingError, with: :rescue404
@@ -11,18 +12,23 @@ module ErrorHandlers
 
   private
 
-  def rescue403(e)
-    @exception = e
-    render 'errors/forbidden', status: 403
-  end
+    def rescue400(e)
+      @exception = e
+      render 'errors/bad_request', status: 400
+    end
 
-  def rescue404(e)
-    @exception = e
-    render 'errors/not_found', status: 404
-  end
+    def rescue403(e)
+      @exception = e
+      render 'errors/forbidden', status: 403
+    end
 
-  def rescue500(e)
-    @exception = e
-    render 'errors/internal_server_error', status: 500
-  end
+    def rescue404(e)
+      @exception = e
+      render 'errors/not_found', status: 404
+    end
+
+    def rescue500(e)
+      @exception = e
+      render 'errors/internal_server_error', status: 500
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module Baukis
       g.controller_specs false
       g.view_specs false
     end
-    config.action_controller.permit_all_parameters = true
   end
 end
